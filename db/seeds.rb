7e8803bf3c8290769ecea0664e7014210653c3a5
Zeljko <zeljko@zwr.fi>
2014-09-18 17:21:45 +0300

    seed events
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 %w{
-  users
+  users events
 }.each do |part|
   load File.expand_path(File.dirname(__FILE__))+"/seeds/#{part}.rb"
 end
---
+++ db/seeds/events.rb
@@ -0,0 +1,15 @@
+p 'Create two events with two masters'
+
+@ec14 = Event.create! name: '2014 IFSS European Championships Finland',
+                        venue: 'Rautavaara',
+                        start_date: '2014-03-07',
+                        end_date: '2014-03-09'
+
+@ec14.masters << @zm << @kp
+
+@tk15 = Event.create! name: '2015 Taivalvaara Sled Dog Race',
+                      venue: 'Taivalvaara, Taivalkoski',
+                      start_date: '2015-03-06',
+                      end_date: '2015-03-08'
+
+@tk15.masters << @zm << @kp
