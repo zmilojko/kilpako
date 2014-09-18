a331a62735f592d6247200f19e69e69a2f162243
Zeljko <zeljko@zwr.fi>
2014-09-18 09:49:56 +0300

    login/out shortcuts
    /login to login, /logout (get) to logout.
    
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
   root 'main#get'
 end
