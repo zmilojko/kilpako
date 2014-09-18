adca0938ab4c3517ee4ff4b3e921feadd58c0964
Zeljko <zeljko@zwr.fi>
2014-09-18 16:50:21 +0300

    fix: allow name set/change for devise routes
    Devise performs sanitation on posted parameters to
    at most email and password. We want to allow user to set
    and change her name as well.
    
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    # sign in takes only email and password
+    # devise_parameter_sanitizer.for(:sign_in) 
+    # when signing up, or changing the account, name can also be set/changed
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
 end
