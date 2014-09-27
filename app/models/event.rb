29f12036c65042330ec5e10acd843ce926a8881f
Zeljko <zeljko@zwr.fi>
2014-09-27 12:59:55 +0300

    event period
    Shows the period when the event takes place, if one day
    then shows the date, otherwise shows the date-span:
    
    1.7.2015
    1.-4.7.2015
    29.6.-1.7.2015
    
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,22 @@ class Event < ActiveRecord::Base
   has_many :event_pages
 
   Status = [:not_published, :open, :ongoing, :closed, :cancelled]
-  
-  
-  
+
   def period_formatted()
-    start_date
+    if start_date.year == end_date.year
+      if start_date.month == end_date.month
+        if start_date.day == end_date.day
+          s="#{end_date.day}.#{end_date.month}."
+        else
+          s="#{start_date.day}.-#{end_date.day}.#{end_date.month}."
+        end
+      else
+        s="#{start_date.day}.#{start_date.month}.-#{end_date.day}.#{end_date.month}."
+      end
+    else
+      s="#{start_date.day}.#{start_date.month}.#{start_date.year}-#{end_date.day}.#{end_date.month}."
+    end
+    s += "#{end_date.year}" # if end_date.year != Time.now.year
+    s
   end
 end
